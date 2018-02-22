d67a66a9e303f58e817535fdea2710d5fa7ddda2
Glen Goulter <glenrg@gmail.com>
2018-02-22 09:50:25 -0800

    second version with fancy salted and hashed PWs using bcrypt

--- apps/logreg_app/views.py
+++ apps/logreg_app/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from .models import User
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
+import bcrypt
+
 # the index function is called when root is visited
 
 #1
@@ -11,7 +13,8 @@ def index(request):
   return render(request,"logreg_app/registrationForm.html")
 
 def doregister(request):
-    u1 = User(first_name = request.POST['first_name'], last_name = request.POST['last_name'], email = request.POST['email'], password = request.POST['password'])
+    # u1 = User(first_name = request.POST['first_name'], last_name = request.POST['last_name'], email = request.POST['email'],     password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()))
+    u1 = User(first_name = request.POST['first_name'], last_name = request.POST['last_name'], email = request.POST['email'],     password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()))
     u1.save()
     # request.session['id']=u1.id
     request.session['first_name']=u1.first_name
@@ -19,28 +22,23 @@ def doregister(request):
 
 def login(request):
     post_password = request.POST['password']
+    # bcrypt.hashpw('test'.encode(), bcrypt.gensalt())
     post_email = request.POST["email"]
     print "*****************post_email " + post_email
     print "*****************post_pass " + post_password
  
-	# user = mysql.query_db(query, data) # []
-	
-    # u = User.objects.get(email=post_email)
-    # if  u.email == post_email:
     try:
         u = User.objects.get(email = post_email)
         print "****************successful try"
-        # u = User.objects.get(email = post_email)
+       
         u.save()
         print u.id
-        print "stored pw " + u.password
-        print "posted pw " + post_password
-        if u.password == post_password:
+        
+        if bcrypt.checkpw(post_password.encode(), u.password.encode()):
             print "password match"
             request.session['id']=u.id
             print "*********** "
             print request.session['id']
-            # print "***********session" + request.session['id']
             return redirect('/dashboard')
         return redirect('/')
     except:
